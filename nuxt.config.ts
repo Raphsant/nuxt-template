e6e7aad494d78baf19a4bffc6fdd4e32a84fe8d5
Rafael Santana <rsantana@colum.edu>
2024-02-25 17:29:43 -0600

    1.1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
+// @ts-ignore
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss'
   ],
+  // @ts-ignore
   extends: [
     'github:Raphsant/nuxt-template',        // GitHub Remote Source
 
